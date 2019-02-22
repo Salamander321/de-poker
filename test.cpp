dd0811a6a60e42768aae8cac54f09c2992ecc233
Salamander321 <poudelpranav@gmail.com>
2019-02-22 14:41:43 +0545

    created a window and menu class but required font is not added yet

--- test.cpp
+++ test.cpp
@@ -2,3 +2,19 @@
 // Created by salamander on 2/21/19.
 //
 
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Menu.h"
+#include "windows.h"
+
+sf::Vector2u tempSize(800,600);
+
+int main(){
+
+    Menu mymenu("DePoker",tempSize);
+    while(mymenu.menuOpen()){
+        mymenu.render();
+
+    }
+    return 0;
+}
